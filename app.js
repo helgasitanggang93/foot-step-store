758ec70cd1d4d3a5949d90a626c9f26d51aaa9d0
sukmaranggapradeta <adit.netral@gmail.com>
2019-04-26 04:52:53 +0700

    2

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT = 5000
+const PORT = process.env.PORT || 4000
 const bodyParser = require('body-parser')
 const session = require('express-session') 
 
